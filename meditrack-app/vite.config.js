840da82eb512559f9baeec1805e3ea7c4c6a2e9e
E Spaeth <edspaeth@yahoo.com>
2025-11-24 21:59:56 -0600

    didn't add complete repo name to vite.config

--- meditrack-app/vite.config.js
+++ meditrack-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `launchcode-unit-1-project-Eric-S`
+  base: `espa85/launchcode-unit-1-project-Eric-S`
 })
